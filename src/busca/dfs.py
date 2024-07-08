7864d45ce787a0d5774777b120c9783f9ec778a0
NicolasPag <nicolas.202211196@unilasalle.edu.br>
2024-07-08 16:22:26 -0300

    Update dfs.py

--- src/busca/dfs.py
+++ src/busca/dfs.py
@@ -5,3 +5,34 @@
 
 def dfs(graph, start: int, goal: int) -> (int, float, [int]):
     """Busca um caminho entre start e goal usando busca em profundidade."""
+
+    stack = [(start, None)]
+    visitedNodes = {}
+    countNodes = 0
+
+    while stack:
+        currNode, predecessor = stack.pop()
+
+        if goal == currNode:
+            path = [currNode]
+            totalCost = 0
+
+            while predecessor is not None:
+                path.append(predecessor)
+                totalCost += graph[predecessor][1][currNode]
+                currNode = predecessor
+                predecessor = visitedNodes.get(currNode)
+
+            path.reverse()
+
+            break
+
+        if currNode not in visitedNodes:
+            countNodes += 1
+            visitedNodes[currNode] = predecessor
+
+            for neighbor, _ in graph[currNode][1].items():
+                if neighbor not in visitedNodes:
+                    stack.append((neighbor, currNode))
+
+    return (countNodes, totalCost, path)
