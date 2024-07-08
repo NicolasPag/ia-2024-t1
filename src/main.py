a9dc2f16ed35b57b7ce725fda9e082b47f7ef37b
NicolasPag <nicolas.202211196@unilasalle.edu.br>
2024-07-08 17:00:43 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,13 +6,9 @@ from busca import a_star
 
 if __name__ == "__main__":
     grafo = read_graph(r"C:\Users\nicol\OneDrive\Documentos\GitHub\.venv\ia-2024-t1\mapas\mini_map.txt")
-
     print(grafo)
-
-    
-
-    #vertices_avaliados, custo, caminho = a_star(grafo, 1, 100)
-    #print(vertices_avaliados, custo, caminho)
+    vertices_avaliados, custo, caminho = a_star(grafo, 0, 4)
+    print(vertices_avaliados, custo, caminho)
 
 
 
