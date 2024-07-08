ca7754980be5efae9e6cf9f64f7243eaa3fada2f
NicolasPag <nicolas.202211196@unilasalle.edu.br>
2024-07-08 15:43:58 -0300

    Update a_star.py

--- src/busca/a_star.py
+++ src/busca/a_star.py
@@ -1,5 +1,53 @@
 """Implementação do algoritmo A*."""
-
+from queue import PriorityQueue
+from util import haversine
 
 def a_star(graph, start: int, goal: int) -> (int, float, [int]):
     """Busca em graph, um caminho entre start e goal usando A*."""
+
+    frontier = PriorityQueue()
+    frontier.put((0, start))
+
+    cameFrom = {}
+    cameFrom[start] = None
+
+    constSoFar = {}
+    constSoFar[start] = None
+
+    countNodes = 0
+
+    while not frontier.empty():
+        _, current = frontier.get()
+        countNodes += 1
+
+        if current == goal:
+            predecessor = cameFrom[current]
+            path = [current]
+
+            while predecessor is not None:
+                path.append(predecessor)
+                predecessor = cameFrom[predecessor]
+
+            path.reverse()
+
+            break
+
+        for nextNode, nextNodeCost in graph[current][1].items():
+            newCost = constSoFar[current] + nextNodeCost
+
+            if (
+                nextNode not in constSoFar or
+                newCost < constSoFar[nextNode]
+            ):
+                constSoFar[nextNode] = newCost
+                priority = newCost + haversine(
+                    graph[goal][0][0],
+                    graph[goal][0][1],
+                    graph[nextNode][0][0],
+                    graph[nextNode][0][1]
+                )
+
+                frontier.put((priority, nextNode))
+                cameFrom[nextNode] = current
+
+    return (countNodes, constSoFar[goal], path)
