7e5ffbfc0b2dfac24290698773cea8e54247f65a
NicolasPag <nicolas.202211196@unilasalle.edu.br>
2024-07-08 14:00:35 -0300

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -6,27 +6,32 @@ import sys
 def read_graph(filename: str):
     """Le uma estrutura de grafo de um arquivo e retorna a estrutura."""
     graph = []
-    with open(filename, "rt") as input_file:
+    with open(filename, "rt", encoding="utf-8") as input_file:
         vertex_count = int(input_file.readline().strip())
         for _ in range(vertex_count):
             index, latitude, longitude = input_file.readline().strip().split()
 
-            index = int(index)
-            latitude = float(latitude)
-            longitude = float(longitude)
+            index, latitude, longitude = [
+                int(index),
+                float(latitude),
+                float(longitude)
+            ]
 
-            graph.append([(latitude, longitude),{}])
+            graph.append([(latitude, longitude), {}])
             
         edge_count = int(input_file.readline().strip())
+
         for _ in range(edge_count):
             from_vertex, to_vertex, cost = (
                 input_file.readline().strip().split()
             )
 
-            from_vertex = int(from_vertex)
-            to_vertex = int(to_vertex)
-            cost = float(cost)
-
+            from_vertex, to_vertex, cost = [
+                int(from_vertex),
+                int(to_vertex),
+                float(cost)
+            ]
+            
             graph[from_vertex][1][to_vertex] = cost
     
     return graph
