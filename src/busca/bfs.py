57e5ea6224ca3477d6bffbbaaac76bb7921715fb
NicolasPag <nicolas.202211196@unilasalle.edu.br>
2024-07-08 16:00:15 -0300

    Update bfs.py

--- src/busca/bfs.py
+++ src/busca/bfs.py
@@ -8,14 +8,35 @@ from queue import deque as Queue
 def bfs(graph, start: int, goal: int) -> (int, float, [int]):
     """Busca um caminho entre start e goal usando busca em largura."""
     
-    try:
-        graph[start]
-        graph[goal]
-
-    except KeyError:
-        print("Erro")
-        return None
-    
     stack = [(start, None)]
+    visitedNodes = {}
 
-    
+    countNodes = 0
+
+    while stack:
+        currNode, predecessor = stack.pop(0)
+
+        if goal == currNode:
+            path = [currNode]
+            totalCost = 0
+
+            while predecessor is not None:
+                path.append(predecessor)
+                totalCost += graph[predecessor][1][currNode]
+                currNode = predecessor
+                predecessor = visitedNodes.get(currNode)
+
+            path.reverse()
+
+            break
+
+        if currNode not in visitedNodes:
+
+            countNodes += 1
+            visitedNodes[currNode] = predecessor
+
+            for neighbor, _ in graph[currNode][1].items():
+                if neighbor not in visitedNodes:
+                    stack.append((neighbor, currNode))
+
+    return ((countNodes, totalCost, path))
