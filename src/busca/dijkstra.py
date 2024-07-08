89baf0a50c9cd343d5a7eeeb70ac4b7b6fb7eb10
NicolasPag <nicolas.202211196@unilasalle.edu.br>
2024-07-08 16:51:07 -0300

    Update dijkstra.py

--- src/busca/dijkstra.py
+++ src/busca/dijkstra.py
@@ -7,3 +7,74 @@ from heapq import heapify, heappush, heappop
 
 def dijkstra(graph, start: int, goal: int) -> (int, float, [int]):
     """Busca em graph, um caminho entre start e goal usando Dijkstra."""
+
+    stackNodes= []
+    processedNodes = []
+
+    heappush(stackNodes, (0, start))
+
+    tempNodes = {}
+    tempNodes[start] = {
+        'predecessor' : None,
+        'currWeight' : 0,
+        'allPredec' : [None]
+    }
+
+    while stackNodes:
+
+        _, node = heappop(stackNodes)
+
+        if node in processedNodes:
+            continue
+
+        processedNodes.append(node)
+
+        if node == goal:
+
+            path = [goal]
+            currPath = goal
+
+            while path[-1] != start:
+                currPath = tempNodes[currPath]['predecessor']
+                path.append(currPath)
+
+            path.reverse()
+
+            break
+
+        visitedNode = tempNodes[node]
+
+        for neighborNode, neighborWeight in graph[node][1].items():
+
+            if (
+                neighborNode not in tempNodes or 
+                node not in tempNodes[neighborNode]['allPredec']
+            ):
+                newWeight = visitedNode['currWeight'] + neighborWeight
+                newPredec = node
+
+                if neighborNode not in tempNodes:
+
+                    heappush(stackNodes, (newWeight, neighborNode))
+                    tempNodes[neighborNode] = {
+                        'predecessor' : newPredec,
+                        'currWeight' : newWeight,
+                        'allPredec' : [newPredec]
+                    }
+
+                elif newWeight < tempNodes[neighborNode]['currWeight']:
+
+                    heappush(stackNodes, (newWeight, neighborNode))
+                    newAllPredec = tempNodes[neighborNode]['allPredec']
+                    newAllPredec.append(newPredec)
+
+                    tempNodes[neighborNode] = {
+                        'predecessor' : newPredec,
+                        'currWeight' : newWeight,
+                        'allPredec' : newAllPredec
+                    }
+
+                else:
+                    tempNodes[neighborNode]['allPredec'].append(newPredec)
+
+    return (len(processedNodes), tempNodes[node]['currWeight'], path)
